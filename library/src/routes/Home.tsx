4ce0f936967284c43853de8d16f68e0b4d6453ce
Luiz Matheus <luizmatheus9570@gmail.com>
2025-06-04 17:14:13 -0300

    Novas features

--- library/src/routes/Home.tsx
+++ library/src/routes/Home.tsx
@@ -1,11 +1,12 @@
 import type { JSX } from "react";
 import { useNavigate } from "react-router-dom";
+import mainSapiens from "../assets/mainSapiens.png";
 
 const Home = (): JSX.Element => {
   const navigate = useNavigate();
   return (
     <section className="w-full min-h-[calc(100%-100px)] max-h-auto flex items-center justify-center flex-col">
-      <div className="w-[90%] flex items-center justify-around flex-col mt-[100px] h-[305px]">
+      <div className="w-[90%] flex items-center justify-around flex-col mt-[100px] min-h-[305px] max-h-[1005px]">
         <span className="text-[72px] color-[#03151e] font-[900] text-center">
           Catalogação em Nuvem
         </span>
@@ -20,7 +21,15 @@ const Home = (): JSX.Element => {
           Começar
         </button>
       </div>
-      <section className=" w-full h-[915px] bg-[#4bc1d2]"></section>
+      <section className="w-full h-[915px] bg-[#cff2f5] mt-[220px] flex items-center justify-center gap-[100px] relative">
+        <div className="w-full h-full relative flex items-center justify-center z-1">
+          <img
+            src={mainSapiens}
+            alt="home"
+            className="aboslute bottom-[250px]"
+          />
+        </div>
+      </section>
     </section>
   );
 };
